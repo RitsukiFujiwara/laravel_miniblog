fb01b30e2476ea67a0c17dcb4bb5e7303655af85
Fujiwara <r9.20important@icloud.com>
2021-12-06 09:15:34 +0900

    画像投稿・画像削除機能追加

--- resources/views/mypage/blog/edit.blade.php
+++ resources/views/mypage/blog/edit.blade.php
@@ -13,7 +13,11 @@
     本文：<textarea name="body" style="width: 600px; height: 200px;">{{ data_get($data, 'body') }}</textarea>
     <br>
     公開する：<label><input type="checkbox" name="is_open" value="1" {{ (data_get($data, 'is_open') ? 'checked' : '') }}>公開する</label>
-
+    <br>
+    画像：<input type="file" name="pict">
+    @if($blog->pict)
+        <p><img src="{{ Storage::url($blog->pict) }}" alt="" width="200"></p>
+    @endif
     <br><br>
 
     <input type="submit" value="更新する">
